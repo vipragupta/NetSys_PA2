c1e4fe94e6d5a79591ac466e1f5701f6199c96a1
vipragupta <guptavipra@gmail.com>
2017-10-22 20:44:51 -0600

    Assignment completed

--- webserver.c
+++ webserver.c
@@ -11,7 +11,6 @@
 
 
 #define MAXLINE 4096 /*max text line length*/
-//#define SERV_PORT 8884/*port*/
 #define LISTENQ 8 /*maximum number of client connections*/
 
 
@@ -31,13 +30,14 @@ FILE *getFilePointer(char filename[])
     return NULL; // error
 }
 
-
+//Function to find the size of sile in bytes
 size_t getFileSize(FILE *file) {
   fseek(file, 0, SEEK_END);
   size_t file_size = ftell(file);
   return file_size;
 }
 
+//This function parses the ws.conf to find the port number from it.
 int getPortNumber() {
   FILE *file;
   file = fopen("ws.conf", "r");
@@ -66,6 +66,7 @@ int getPortNumber() {
   return atoi(port);
 }
 
+//This function parses the ws.conf to find the root Directtory from it.
 void getRootDirectory(char *root) {
   FILE *file;
   file = fopen("ws.conf", "r");
@@ -90,20 +91,54 @@ void getRootDirectory(char *root) {
   fclose(file);
 }
 
+//This function returns the content type of the given request url.
+void getContentType(char *requestUrl, char *contentType) {
+  bzero(contentType, sizeof(contentType));
+  char *fileType = strrchr(requestUrl, '.');
+  if(strlen(fileType) > 0) {
+    if (strcmp(fileType, ".html") == 0) {
+      strcpy(contentType, "text/html");
+    } else if (strcmp(fileType, ".htm") == 0) {
+      strcpy(contentType, "text/html");
+    } else if (strcmp(fileType, ".txt") == 0) {
+      strcpy(contentType, "text/plain");
+    } else if (strcmp(fileType, ".png") == 0) {
+      strcpy(contentType, "image/png");
+    } else if (strcmp(fileType, ".gif") == 0) {
+      strcpy(contentType, "image/gif");
+    } else if (strcmp(fileType, ".jpg") == 0) {
+      strcpy(contentType, "image/jpg");
+    } else if (strcmp(fileType, ".css") == 0) {
+      strcpy(contentType, "text/css");
+    } else if (strcmp(fileType, ".js") == 0) {
+      strcpy(contentType, "text/javascript");
+    } else if (strcmp(fileType, ".ico") == 0) {
+      strcpy(contentType, "image/x-icon");
+    } else {
+      //printf("MAking Content-Type null\n");
+      contentType = NULL;
+    }
+  } else {
+    //printf("MAking Content-Type null\n");
+    contentType = NULL;
+  }
+}
+
+
+//This function parses the ws.conf to find the url from it.
 void getUrl(char *url, char *request) {
   int i = 0;
 
   char *tok = strtok(request, " ");
   if (tok != NULL) {
     char *temp = strtok(NULL, " ");
-    printf("tok URL:%s\n", tok);
-    printf("temp URL:%s\n", temp);
     strcpy(url, temp);
     url[strlen(temp)] = '\0';
   }
 }
 
 
+//This function parses the ws.conf to find the default filename from it.
 void getDefaultFileName(char *address) {
   FILE *file;
   file = fopen("ws.conf", "r");
@@ -128,6 +163,7 @@ void getDefaultFileName(char *address) {
   fclose(file); 
 }
 
+//This function parses the ws.conf to find all the content types that the server handles.
 void getHandledContentType(char *address) {
   FILE *file;
   file = fopen("ws.conf", "r");
@@ -143,7 +179,6 @@ void getHandledContentType(char *address) {
 
         if (tok != NULL) {
           char *temp = strtok(NULL, "\n");
-          //printf("i:%d    ext:%s   type:%s\n", i, tok, temp);
           if (i == 8) {
             strcpy(address, tok);
           } else {
@@ -161,39 +196,57 @@ void getHandledContentType(char *address) {
   fclose(file); 
 }
 
+//The response that should be sent to client in case of 501 error.
 void getFiveOOneResponse(char *responseBuffer, char *requestUrl) {
   bzero(responseBuffer, sizeof(responseBuffer));
   strcpy(responseBuffer, "HTTP/1.1 501 Not Implemented\r\n");
   strcat(responseBuffer, "Content-Type: text/html \r\n");
-  strcat(responseBuffer,"Content-Length:100");
+  strcat(responseBuffer,"Content-Length:200");
   strcat(responseBuffer, "\r\n\r\n");
   strcat(responseBuffer,"<html><body>501 Not Implemented  <br/><b>Url: ");
   strcat(responseBuffer, requestUrl);
   strcat(responseBuffer, "</b></body></html>");
 }
 
+//The response that should be sent to client in case of 404 error.
 void getFourOFourResponse(char *responseBuffer, char *requestUrl) {
+  //printf("RequestURL--%s\n", requestUrl);
   bzero(responseBuffer, sizeof(responseBuffer));
   strcpy(responseBuffer, "HTTP/1.1 404 Not Found\r\n");
   strcat(responseBuffer, "Content-Type: text/html \r\n");
-  strcat(responseBuffer,"Content-Length:100");
+  strcat(responseBuffer,"Content-Length:200");
   strcat(responseBuffer, "\r\n\r\n");
   strcat(responseBuffer,"<html><body>404 Not Found Reason URL doesn not exist<br/><b>Url: ");
   strcat(responseBuffer, requestUrl);
   strcat(responseBuffer, "</b></body></html>");
 }
 
+//The response that should be sent to client in case of 500 error.
 void getFiveHundreadResponse(char *responseBuffer, char *requestUrl) {
   bzero(responseBuffer, sizeof(responseBuffer));
   strcpy(responseBuffer, "HTTP/1.1 500 Internal Server Error\r\n");
   strcat(responseBuffer, "Content-Type: text/html \r\n");
-  strcat(responseBuffer,"Content-Length:100");
+  strcat(responseBuffer,"Content-Length:200");
   strcat(responseBuffer, "\r\n\r\n");
   strcat(responseBuffer,"<html><body>500 Internal Server Error: cannot allocate memory<br/><b>Url: ");
   strcat(responseBuffer, requestUrl);
   strcat(responseBuffer, "</b></body></html>");
 }
 
+
+//The response that should be sent to client in case of 400 error.
+void getFourHundreadResponse(char *responseBuffer, char *reason) {
+  bzero(responseBuffer, sizeof(responseBuffer));
+  strcpy(responseBuffer, "HTTP/1.1 400 Bad Request\r\n");
+  strcat(responseBuffer, "Content-Type: text/html \r\n");
+  strcat(responseBuffer,"Content-Length:400");
+  strcat(responseBuffer, "\r\n\r\n");
+  strcat(responseBuffer,"<html><body><b>400 Bad Request Reason:");
+  strcat(responseBuffer, reason);
+  strcat(responseBuffer, "</b></body></html>");
+}
+
+//To find if the client is aking for a file or a directory.
 int isGetFile(char *url) {
   char *tok = strrchr(url, '.');
   if (tok == NULL) {
@@ -202,22 +255,7 @@ int isGetFile(char *url) {
   return 1;
 }
 
-int isValidFileType(char *requestUrl, char *types) {
-  printf("types: %s\n", types);
-  printf("requestUrl: %s\n", requestUrl);
-  char *fileType = strrchr(requestUrl, '.');
-  //printf("fileType:\n", fileType);
-  char *typePresent = strstr(types, fileType);
-  
-  if (typePresent != NULL) {
-    printf("RETURN 1 fileType:%s    typePresent: %s\n", fileType, typePresent);
-    return 1;
-  } else {
-    printf("RETURN 0 fileType:%s    typePresent: %s\n", fileType, typePresent);
-    return 0;
-  }
-}
-
+//Function to find if the requested file is of image, js, css, gif types
 int isImageTypeFile(char *requestUrl) {
   printf("requestUrl: %s\n", requestUrl);
   char *fileType = strrchr(requestUrl, '.');
@@ -286,30 +324,30 @@ int main (int argc, char **argv)
       //close listening socket
       close(listenfd);
 
+      //While a request is received.
       while ((n = recv(connfd, request, MAXLINE,0)) > 0)  {
         printf("*****************************************************************\n");
         printf("Request received from client\n\n");
         puts(request);
         char *httpRequest;
-        char requestMethod[10];
+        char requestMethod[20];
         char requestUrl[70];
-        char requestVersion[10];
+        char requestVersion[20];
         char urlByToken[70];
 
         httpRequest = strtok(request, "\n");
-        //printf("%s\n", httpRequest);
         int i = 0 ;
         int j = 0;
         int arg = 0;
         char temp[30];
 
+        printf("Http Request: %s\n", httpRequest);
         while (httpRequest[i] != '\0') {
           if (httpRequest[i] == ' ') {
             if (arg == 0) {
               bzero(requestMethod, sizeof(requestMethod));
               strcpy(requestMethod, temp);
             } else if (arg == 1) {
-              printf("temp:%s    len:%lu\n", temp, strlen(temp));
               bzero(requestUrl, sizeof(requestUrl));
               strcpy(requestUrl, temp);
               requestUrl[strlen(temp)] = '\0';
@@ -326,6 +364,10 @@ int main (int argc, char **argv)
         bzero(requestVersion, sizeof(requestVersion));
         memset(requestVersion, '\0', sizeof(requestVersion));
         strcpy(requestVersion, temp);
+        printf("Request Method: %s\n", requestMethod);
+        printf("Request Version:%s\n", requestVersion);
+        printf("Request Url: %s\n", requestUrl);
+
 
         getUrl(urlByToken, httpRequest);
 
@@ -333,27 +375,56 @@ int main (int argc, char **argv)
         int responseIndex = 0;
         char fileBuffer[1048576];
 
+        //printf("\nHey\n  %s\n   ", (strstr(temp, "HTTP/1.1")));
+        //printf("%s\n\n", (strstr(requestVersion, "HTTP/1.0")));
+        //printf("Hello %s  %d\n", requestMethod, strcmp(requestMethod, "GET"));
+        if ((strstr(requestVersion, "HTTP/1.1") == NULL) && (strstr(requestVersion, "HTTP/1.0") == NULL)) {
+          char reason[400];
+          strcpy(reason, "Invalid HTTP-Version ");
+          //strcat(reason, requestVersion);
+          //reason[strlen(reason)] = '\0';
+
+          //printf("reason: %s\n", reason);
+          getFourHundreadResponse(responseBuffer, reason);
+          send(connfd, responseBuffer, sizeof(responseBuffer), 0);
+          continue;
+        }
+
+        //printf("Hello %s  %d\n", requestMethod, strcmp(requestMethod, "GET"));
         if (strcmp(requestMethod, "GET") == 0) {
+
+          char url4[100];
+          strncpy(url4, requestUrl, strlen(requestUrl)); 
           int isSendImage = 0;
           FILE *file;
           char filename[50];
           memset(filename, '\0', sizeof(filename));
 
           int isFile = isGetFile(requestUrl);
-          printf("requestUrl:%s\n", requestUrl);
-          printf("Url:%s\n", urlByToken);
+          //printf("requestUrl:%s\n", requestUrl);
+
           printf("\nISFILE::::%d\n", isFile);
           if (isFile == 1) {
-           
-            int validFileType = isValidFileType(requestUrl, fileType);
-            printf("ISVALIDFILETYPE::::%d\n", validFileType);
-            if (validFileType == 1) {
+            char contentType[100];
+            getContentType(requestUrl, contentType);
+            contentType[strlen(contentType)] = '\0';
+
+            char contentType4[100];
+            strncpy(contentType4, contentType, strlen(contentType)); 
+            contentType4[strlen(contentType)] = '\0';
+
+            printf("CONTENT TYPE::::%s\n", contentType);
+            
+            if (contentType && contentType != NULL && strlen(contentType) > 0) {
               int isImageType = isImageTypeFile(requestUrl);
               printf("ISIMAGETYPE::::%d\n", isImageType);
+              
                 if (isImageType == 0) {
+
                   printf("rootAddress::::%sss\n", rootAddress);
                   strcpy(filename, rootAddress);
                   strcat(filename, requestUrl);
+
                   printf("FILENAME:::%s\n", filename);
                   file = getFilePointer(filename);
 
@@ -361,13 +432,20 @@ int main (int argc, char **argv)
                   {
                      printf("file does not exist\n");
                      bzero(responseBuffer, sizeof(responseBuffer));
-                     getFourOFourResponse(responseBuffer, requestUrl);
+                     getFourOFourResponse(responseBuffer, url4);
                      printf("\nResponse:\n%s\n\n", responseBuffer);
                      send(connfd, responseBuffer, sizeof(responseBuffer), 0);                     
                   } else {
                     bzero(responseBuffer, sizeof(responseBuffer));
-                    strcpy(responseBuffer, "HTTP/1.1 200 OK\r\n");
-                    strcat(responseBuffer, "Content-Type: text/plain \r\n");  //TODO
+                    if ((strstr(temp, "HTTP/1.1")) != NULL) {
+                      strcpy(responseBuffer, "HTTP/1.1 200 OK\r\n");
+                    } else {
+                      strcpy(responseBuffer, "HTTP/1.0 200 OK\r\n");
+                    }
+                    //strcpy(responseBuffer, "HTTP/1.1 200 OK\r\n");
+                    strcat(responseBuffer, "Content-Type: ");
+                    strcat(responseBuffer, contentType);
+                    strcat(responseBuffer, "\r\n");
                     strcat(responseBuffer,"Content-Length:");
                     size_t file_size = getFileSize(file);     //Tells the file size in bytes.
                     fseek(file, 0, SEEK_SET);
@@ -394,7 +472,7 @@ int main (int argc, char **argv)
                   }
                 } else {  // This is when we need to send a image i.e. binary data.
                   //Reference: https://stackoverflow.com/questions/28631767/sending-images-over-http-to-browser-in-c
-                  printf("Inside Send Image\n");
+                  //printf("Inside Send Image\n");
                   struct stat filestat;
                   FILE *fp;
                   int fd;
@@ -404,15 +482,15 @@ int main (int argc, char **argv)
 
                   strcpy(filename, rootAddress);
                   strcat(filename, requestUrl);
-                  printf("FILENAME:::%s\n", filename);
-                  
 
                   if ( ((fd = open (filename, O_RDONLY)) < -1) || (fstat(fd, &filestat) < 0) ) {
-                    printf ("Error in measuring the size of the file");
+                    printf ("Error in Opening the file\n");
                     bzero(responseBuffer, sizeof(responseBuffer));
-                    getFourOFourResponse(responseBuffer, requestUrl);
+
+                    getFourOFourResponse(responseBuffer, url4);
                     printf("\nResponse:\n%s\n\n", responseBuffer);
                     send(connfd, responseBuffer, sizeof(responseBuffer), 0);
+                    continue;
                   }                  
                   sprintf (filesize, "%zd", filestat.st_size);
                   fp = fopen (filename, "r");
@@ -420,17 +498,22 @@ int main (int argc, char **argv)
                   if (fp == NULL) {
                       printf("file does not exist\n");
                       bzero(responseBuffer, sizeof(responseBuffer));
-                      getFourOFourResponse(responseBuffer, requestUrl);
+                      getFourOFourResponse(responseBuffer, url4);
                       printf("\nResponse:\n%s\n\n", responseBuffer);
                       send(connfd, responseBuffer, sizeof(responseBuffer), 0);
+                      continue;
                   } else {
+                    //printf("IMAGE CONTENT TYPE:%s\n", contentType4);
                     strcpy(responseBuffer, "HTTP/1.1 200 OK\r\n");
-                    strcat(responseBuffer, "Content-Type: image/jpg \r\n");  //TODO: Make a function and remove hardcoded.
+                    strcat(responseBuffer, "Content-Type: ");
+                    strcat(responseBuffer, contentType4);
+                    strcat(responseBuffer, "\r\n");
+                    //printf("2IMAGE CONTENT TYPE:%s\n", contentType4);
                     strcat(responseBuffer,"Content-Length:");
                     strcat(responseBuffer, filesize);
                     strcat(responseBuffer, "\r\n");
                     strcat(responseBuffer, "Connection: keep-alive\r\n\r\n");
-
+                    printf("\nResponse:\n%s\n\n", responseBuffer);
                     //Send header content first.
                     write (connfd, responseBuffer, strlen(responseBuffer));
                     fread (file_buff, sizeof(char), filestat.st_size + 1, fp);
@@ -459,13 +542,13 @@ int main (int argc, char **argv)
             {
                 printf("file does not exist\n");
                 bzero(responseBuffer, sizeof(responseBuffer));
-                getFourOFourResponse(responseBuffer, requestUrl);
+                getFourOFourResponse(responseBuffer, url4);
                 printf("\nResponse:\n%s\n\n", responseBuffer);
                 send(connfd, responseBuffer, sizeof(responseBuffer), 0);
                 
             } else {
               strcpy(responseBuffer, "HTTP/1.1 200 OK\r\n");
-              strcat(responseBuffer, "Content-Type: text/html \r\n");  //TODO
+              strcat(responseBuffer, "Content-Type: text/html \r\n");
               strcat(responseBuffer,"Content-Length:");
               size_t file_size = getFileSize(file);     //Tells the file size in bytes.
               fseek(file, 0, SEEK_SET);
